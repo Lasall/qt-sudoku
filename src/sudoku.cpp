10ebe191d1a93a29ff5d0cc2ad598b62bcc58835
Dominique Lasserre <lasserre.d@gmail.com>
2017-08-25 03:17:11 +0200

    sudoku, check input range, throw exception

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -11,18 +11,29 @@ Sudoku::Sudoku() {
 Sudoku::~Sudoku() {
 }
 
-int Sudoku::get_field(const int x, const int y) const {
+int Sudoku::get_field(const int x, const int y) {
+    validate_field_range(x);
+    validate_field_range(y);
     return fields[x][y];
 }
 
 void Sudoku::set_field(const int x, const int y, const int value) {
+    validate_field_range(x);
+    validate_field_range(y);
     fields[x][y] = value;
 }
 
-const int* Sudoku::operator[](const int row) const {
-    return fields[row];
+Sudoku::Column Sudoku::operator[](const int row) {
+    validate_field_range(row);
+    return Column(fields[row]);
 }
 
-int* Sudoku::operator[](const int row) {
-    return fields[row];
+int& Sudoku::Column::operator[](int column) {
+    validate_field_range(column);
+    return fields[column];
+}
+
+void Sudoku::validate_field_range(const int field) {
+    if (field < 0 || field > 8)
+        throw std::out_of_range(std::to_string(field) + " != [0-8]");
 }
--- src/sudoku.h
+++ src/sudoku.h
@@ -1,18 +1,30 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
+#include <stdexcept>
+#include <string>
 
 class Sudoku {
  public:
     Sudoku();
     ~Sudoku();
-    int get_field(const int x, const int y) const;
-    void set_field(const int x, const int y, const int value);
 
-    const int* operator[](const int row) const;
-    int* operator[](const int row);
+    int get_field(const int x, const int y);  // throws std::out_of_range
+    void set_field(const int x, const int y, const int value);  // throws std::out_of_range
+
+    class Column {
+     public:
+         Column(int fields[]) : fields(fields) {}
+         int& operator[](int column);  // throws std::out_of_range
+
+     private:
+         int* fields;
+    };
+
+    Column operator[](const int row);  // throws std::out_of_range
 
  private:
     int fields[9][9];
+    static void validate_field_range(const int field);  // throws::std::out_of_range
 };
 
 #endif  // SUDOKU_H
