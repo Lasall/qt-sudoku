08c6906814e201f7efbb83f461dcd9328a9cf36e
Dominique Lasserre <lasserre.d@gmail.com>
2017-09-05 20:40:25 +0200

    LineSolver::solve run all algorithms until no new number found.

--- src/line_solver.cpp
+++ src/line_solver.cpp
@@ -7,20 +7,26 @@ LineSolver::~LineSolver() {
 }
 
 bool LineSolver::solve(Sudoku& s) {
-    bool found = false;
+    bool found_orig = false;
 
-    for (int i = 0; i < 9; ++i) {
-        if (solve_line(s.begin_row(i), s.end_row(i)))
-            found = true;
-    }
-    for (int i = 0; i < 9; ++i) {
-        if (solve_line(s.begin_column(i), s.end_column(i)))
-            found = true;
-    }
-    for (int i = 0; i < 9; ++i) {
-        if (solve_line(s.begin_square(i), s.end_square(i)))
-            found = true;
-    }
+    bool found;
+    do {
+        found = false;
+        for (int i = 0; i < 9; ++i) {
+            if (solve_line(s.begin_row(i), s.end_row(i)))
+                found = true;
+        }
+        for (int i = 0; i < 9; ++i) {
+            if (solve_line(s.begin_column(i), s.end_column(i)))
+                found = true;
+        }
+        for (int i = 0; i < 9; ++i) {
+            if (solve_line(s.begin_square(i), s.end_square(i)))
+                found = true;
+        }
+        if (found)
+            found_orig = true;
+    } while (found);
 
-    return found;
+    return found_orig;
 }
